bdb797dad895f3c7db15e6c5c2658fe54620ad4a
Sagar Vemuri <svemuri@twitter.com>
2015-05-04 23:12:15 -0700

    Initial Commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Sagar Vemuri on 5/4/15.
+//  Copyright (c) 2015 Sagar Vemuri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
